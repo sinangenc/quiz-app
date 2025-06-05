afcb23665b13e09b7d8541d89486a8063fa26bd4
sinangenc <38430115+sinangenc@users.noreply.github.com>
2025-06-05 10:33:52 +0200

    Update DashboardSidebar

--- quiz-app-frontend/app/_components/DashboardSidebar/DashboardSidebar.jsx
+++ quiz-app-frontend/app/_components/DashboardSidebar/DashboardSidebar.jsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import RoleControl from '@/app/_components/RoleControl/RoleControl';
 
 export default function DashboardSidebar(){
     const pathname = usePathname();
@@ -30,6 +31,23 @@ export default function DashboardSidebar(){
                     </Link>
                 </div>
 
+                <RoleControl allowedRoles={["ADMIN"]}>
+                   <hr className="border-gray-300" />
+                    {/* Admin Group */}
+                    <div>
+                        <h5 className="text-link dark:text-white/70 caption font-semibold leading-6 tracking-widest text-xs pb-2 uppercase">
+                            Admin Operations
+                        </h5>
+                        <Link href="/dashboard/admin/questions" className={linkClass('/dashboard/admin/questions')}>
+                            Questions
+                        </Link>
+                        <Link href="/dashboard/admin/users" className={linkClass('/dashboard/admin/users')}>
+                            Users
+                        </Link>
+                    </div>
+                </RoleControl>
+
+                
                 <hr className="border-gray-300" />
                 
                 {/* Quiz Group */}
